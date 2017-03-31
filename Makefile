80815b22994ef970e80ae078e0f3df1d98cb733f
Jakub Stejskal <stejskinek@gmail.com>
2017-04-01 01:54:18 +0200

    Upraveno Makefile, přidány testy, make test spustí testy

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@
 # Soubor: Makefile
 # Popis: Skript pro přeložení vytvořené aplikace v jazyce Haskell
 
+LOGIN = xstejs24
+PACK = Parser/*.hs Type/*.hs *.hs Makefile Readme.txt tests
 
 all:
 	ghc --make rv-2-rka.hs
@@ -15,4 +17,11 @@ rut:
 	./rv-2-rka -t
 
 clean:
-	rm -rf rv-2-rka *.o *.hi Parser/*.o Parser/*.hi Type/*.o Type/*.hi
+	rm -rf rv-2-rka *.o *.hi Parser/*.o Parser/*.hi Type/*.o Type/*.hi Tests/diff Tests/output
+
+test:
+	./Tests/test.sh
+
+pack:
+	rm -f $(LOGIN).zip
+	zip -r $(LOGIN).zip $(PACK)
---
+++ Tests/test.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Projekt do předmětu FLP
+# Převod RV na RKA
+# Autor: Bc. Jakub Stejskal
+# Soubor: test.hs
+# Popis: Skript pro testování vytvořeného převodníku
+
+red='\033[1;31m'
+green='\033[1;32m'
+NC='\033[0m'
+
+INPUT=input/test
+OUTPUT=output/output
+REF=ref_out/ref_out
+
+# Odstranění staré binárky a předchozích výstupů
+rm -r Tests/diff
+rm -r Tests/output
+
+# cd ../
+# Ověření existence makefile a rv-2-rka
+if [ ! -e Makefile ]; then
+	echo -e "${red}Nenalezeno Makefile!"
+	exit 1
+fi
+
+if [ ! -e rv-2-rka.hs ]; then
+	echo -e "${red}Nenalezen soubor 'rv-2-rka.hs'!"
+	exit 1
+fi
+
+
+# Překlad
+make clean
+make
+
+# Kontrola překladu
+if [[ $? -ne 0 ]] ; then
+	echo -e "${red}Nelze přeložit!"
+    exit 1
+fi
+
+# Vstup do podsložky s testy
+cd Tests
+mkdir diff
+mkdir output
+
+# Testování
+x=$(ls input/ -1 | wc -l)	# Počet testů
+for ((i=1; i <= x; i++))
+do
+	./../rv-2-rka -t ${INPUT}$i > ${OUTPUT}$i
+	diff -u ${REF}$i ${OUTPUT}$i > diff/diff$i
+	if [ $? = 0 ]
+		then
+			echo -e "${green}Test ${i} PASS! -> in: $(cat ${INPUT}$i)"
+		else
+			echo -e "${red}Test ${i} FAIL -> in: $(cat ${INPUT}$i) - check diff/diff$i"
+		fi
+		echo -e "${NC}----------------------"
+done
+
+# Progress bar
+# echo -ne '#####                     (33%)\r'
+# sleep 1
+# echo -ne '#############             (66%)\r'
+# sleep 1
+# echo -ne '#######################   (100%)\r'
+# echo -ne '\n'
